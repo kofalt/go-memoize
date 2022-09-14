6eb06c413e5a73b5761fc0e1f0c3ccbca3413837
Nathaniel Kofalt <nathaniel@kofalt.com>
2022-09-14 08:22:05 -0500

    Update to go 1.19

--- memoize_test.go
+++ memoize_test.go
@@ -9,73 +9,13 @@ import (
 	"github.com/smartystreets/gunit"
 )
 
-/*
-	Testing goals, in order:
-
-	1) automatically parallel on the terminal
-	2) zero-ish overhead
-	3) tolerable syntax
-
-	For goal #1, Gunit seems to be the best-equipped to handle things, and there's some agreement on that:
-
-	> I know that I'm the creator of GoConvey and all, but I've actually moved to gunit,
-	> which uses t.Parallel() under the hood for every test case. - @mdwhatcott
-	> https://github.com/smartystreets/goconvey/issues/360
-
-	Test requirements:
-
-	1) Each test works independent of any preexisting state, or lack thereof.
-	2) Ideally tests can clean up after themselves, but this is not required.
-
-	Please keep these goals and requirements in mind when modifying this package.
-*/
-
-/*
-
-	A possible future goal:
-	4) ability to both hit a testing infra, and/or replay locally.
-
-	The best plan seems to be to incorporate go-vcr:
-	https://github.com/dnaeon/go-vcr
-
-	The implementation throws requests in YAML files, which... eh, let's try it maybe.
-	There will have to be some setup trickery to transparently hit live or recorded.
-	I think the vcr transport should handle that.
-*/
-
-// TestSuite fires off gunit.
-//
-// Gunit will look at various function name prefixes to determine behavior:
-//
-//   "Test": Well, it's a test.
-//   "Skip": Skipped.
-//   "Long": Skipped when `go test` is ran with the `short` flag.
-//
-//   "Setup":    Executed before each test.
-//   "Teardown": Executed after  each test.
-//
-// Functions without these prefixes are ignored.
 func TestSuite(t *testing.T) {
 	gunit.Run(new(F), t)
 }
 
-// F is the default fixture, so-named for convenience.
 type F struct {
 	*gunit.Fixture
 }
-
-// Setup prepares the fixture. Runs once per test.
-func (t *F) Setup() {
-
-}
-
-/*
-// An example test:
-func (t *F) TestExample() {
-	t.So(42, ShouldEqual, 42)
-	t.So("Hello, World!", ShouldContainSubstring, "World")
-}
-*/
 
 // TestBasic adopts the code from readme.md into a simple test case
 func (t *F) TestBasic() {
